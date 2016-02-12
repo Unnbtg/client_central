24505746b80ff023835a4ed498b814d11215d613
btgmarco33@gmail.com <nK98nxiZ>
2016-02-12 13:59:17 -0200

    [teste] teste

---
+++ src/MsiClient/Base.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 12/02/16
+ * Time: 13:54
+ */
+
+namespace MsiClient;
+
+
+class Base
+{
+
+
+    public function teste($teste) {
+        echo $teste;
+    }
+
+}
