f00528e5718b3c07efebe6389c267436ca6987e5
btgmarco33@gmail.com <nK98nxiZ>
2016-02-12 11:29:41 -0200

    [teste] teste

---
+++ src/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 12/02/16
+ * Time: 11:27
+ */
+
+namespace Client;
+
+
+class Base
+{
+    public function teste($valor) {
+        echo $valor;
+    }
+}
