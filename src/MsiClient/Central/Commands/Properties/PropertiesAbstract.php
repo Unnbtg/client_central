e59ef4125fc1aab3b6500369c39372fcb63ca9f4
Unm <btgmarco33@gmail.com>
2016-07-21 19:19:36 -0300

    [FIX] User

--- src/MsiClient/Central/Commands/Properties/PropertiesAbstract.php
+++ src/MsiClient/Central/Commands/Properties/PropertiesAbstract.php
@@ -1,53 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marco
- * Date: 26/02/2016
- * Time: 10:20
- */
+    /**
+     * Created by PhpStorm.
+     * User: Marco
+     * Date: 26/02/2016
+     * Time: 10:20
+     */
 
-namespace MsiClient\Central\Commands\Properties;
+    namespace MsiClient\Central\Commands\Properties;
 
-abstract class PropertiesAbstract implements Sendable
-{
-    public function toArray()
+    abstract class PropertiesAbstract implements Sendable
     {
-        $retorno = [];
-        foreach ($this as $key => $property) {
-            $retorno[$key] = $property;
-        }
-
-        return $retorno;
-    }
-
-    public function fromStdClass($std)
-    {
-        return $this->fromJsonElement($std);
-    }
-
-    protected function fromJsonElement($elements)
-    {
-        foreach ($elements as $name => $value) {
-            if (in_array($name, ['created_at', 'updated_at', 'deleted_at']) && is_string($value)) {
-                $this->$name = new \DateTime($value);
-                continue;
+        public function toArray()
+        {
+            $retorno = [];
+            foreach ($this as $key => $property) {
+                $retorno[$key] = $property;
             }
-            $this->$name = $value;
+
+            return $retorno;
         }
 
-        return $this;
-    }
-
-    public function __get($name)
-    {
-        if (!isset($this->$name)) {
-            return null;
+        public function fromStdClass($std)
+        {
+            return $this->fromJsonElement($std);
         }
 
-        return $this->$name;
-    }
+        protected function fromJsonElement($elements)
+        {
+            foreach ($elements as $name => $value) {
+                if (in_array($name, ['created_at', 'updated_at', 'deleted_at']) && is_string($value)) {
+                    $this->$name = new \DateTime($value);
+                    continue;
+                }
+                $this->$name = $value;
+            }
 
-    public function getContainer() {
-        return "";
+            return $this;
+        }
+
+        public function __get($name)
+        {
+            if ( ! isset($this->$name)) {
+                return null;
+            }
+
+            return $this->$name;
+        }
+
+        public function getContainer()
+        {
+            return "";
+        }
+
+
+        public function massAssignment($array)
+        {
+            foreach ($array as $key => $value) {
+                $this->{$key} = $value;
+            }
+        }
     }
-}
--- src/MsiClient/Central/Commands/User.php
+++ src/MsiClient/Central/Commands/User.php
@@ -8,10 +8,20 @@
 
     namespace MsiClient\Central\Commands;
 
+    use MsiClient\Central\Commands\Properties\UserProperties;
 
     class User extends Command
     {
         public $url = '/user';
+
+        public function __construct(\MsiClient\Client $client = null)
+        {
+            if ( ! is_null($client)) {
+                $this->setClient($client);
+            }
+
+        }
+
 
         public function getVendor()
         {
@@ -31,5 +41,45 @@
             }
         }
 
+
+        public function getUser($code)
+        {
+            try {
+                return $this->perform([], \MsiClient\Client::GET_REQUEST, $this->getUrl() . '/' . $code);
+            } catch (\Exception $e) {
+                throw  $e;
+            }
+        }
+
+        public function store($user)
+        {
+            $userProperties = new UserProperties();
+
+            if (is_array($user)) {
+                $userProperties->massAssignment($user);
+            }
+
+            $type = empty($userProperties->id) ? \MsiClient\Client::POST_REQUEST : \MsiClient\Client::PUT_REQUEST;
+
+            return $this->storeRequest($this->getUrl() . '/' . $userProperties->id, $type, $userProperties);
+
+        }
+
+        public function dataTable($data)
+        {
+            try {
+                return $this->perform($data, \MsiClient\Client::POST_REQUEST, $this->getUrl().'/datatable');
+            } catch (\Exception $e) {
+                throw  $e;
+            }
+        }
+
+        public function destroy($id) {
+            try {
+                return $this->perform([], \MsiClient\Client::DELETE_REQUEST, $this->getUrl().'/'.$id);
+            } catch (\Exception $e) {
+                throw  $e;
+            }
+        }
 
     }
