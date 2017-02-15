7fa1da58aa2e25a5cf78c85dff75bb55c760aee7
Marco augusto <btgmarco33@gmail.com>
2017-02-15 16:17:42 -0200

    [V2]

--- src/MsiClient/Central/Commands/UserCommand.php
+++ src/MsiClient/Central/Commands/UserCommand.php
@@ -10,13 +10,13 @@ namespace MsiClient\Central\Commands;
 
 use MsiClient\Central\Commands\Properties\PermissionProperties;
 use MsiClient\Central\Commands\Properties\UserProperties;
-
+use \MsiClient\Client as FuckAtAll;
 
 class UserCommand extends Command
 {
     public $url = '/user';
 
-    public function __construct(\MsiClient\Client $client = null)
+    public function __construct(FuckAtAll $client = null)
     {
         if (!is_null($client)) {
             $this->setClient($client);
@@ -26,7 +26,7 @@ class UserCommand extends Command
     public function getVendor()
     {
         try {
-            return $this->perform([], \MsiClient\Client::GET_REQUEST, $this->getUrl() . '/vendor');
+            return $this->perform([], FuckAtAll::GET_REQUEST, $this->getUrl() . '/vendor');
         } catch (\Exception $e) {
             throw  $e;
         }
@@ -35,7 +35,7 @@ class UserCommand extends Command
     public function getCustomer()
     {
         try {
-            return $this->perform([], \MsiClient\Client::GET_REQUEST, $this->getUrl() . '/customer');
+            return $this->perform([], FuckAtAll::GET_REQUEST, $this->getUrl() . '/customer');
         } catch (\Exception $e) {
             throw  $e;
         }
@@ -45,7 +45,7 @@ class UserCommand extends Command
     public function getUser($code)
     {
         try {
-            return $this->perform([], \MsiClient\Client::GET_REQUEST, $this->getUrl() . '/' . $code);
+            return $this->perform([], FuckAtAll::GET_REQUEST, $this->getUrl() . '/' . $code);
         } catch (\Exception $e) {
             throw  $e;
         }
@@ -59,7 +59,7 @@ class UserCommand extends Command
             $userProperties->massAssignment($user);
         }
 
-        $type = empty($userProperties->id) ? \MsiClient\Client::POST_REQUEST : \MsiClient\Client::PUT_REQUEST;
+        $type = empty($userProperties->id) ? FuckAtAll::POST_REQUEST : FuckAtAll::PUT_REQUEST;
         $url = empty($userProperties->id) ? $this->getUrl() : $this->getUrl() . '/' . $userProperties->id;
 
 
@@ -69,7 +69,7 @@ class UserCommand extends Command
     public function dataTable($data)
     {
         try {
-            return $this->perform($data, \MsiClient\Client::POST_REQUEST, $this->getUrl() . '/datatable');
+            return $this->perform($data, FuckAtAll::POST_REQUEST, $this->getUrl() . '/datatable');
         } catch (\Exception $e) {
             throw  $e;
         }
@@ -78,7 +78,7 @@ class UserCommand extends Command
     public function destroy($id)
     {
         try {
-            return $this->perform([], \MsiClient\Client::DELETE_REQUEST, $this->getUrl() . '/' . $id);
+            return $this->perform([], FuckAtAll::DELETE_REQUEST, $this->getUrl() . '/' . $id);
         } catch (\Exception $e) {
             throw  $e;
         }
@@ -91,19 +91,19 @@ class UserCommand extends Command
 
     public function syncPermissions($userId, $permissions)
     {
-        $formatter = \MsiClient\Central\Factory\Formatter::create(\MsiClient\Client::Formart_Request);
+        $formatter = \MsiClient\Central\Factory\Formatter::create(FuckAtAll::Formart_Request);
         return $this->perform(['data' => $formatter->encode(['permission' => $permissions])],
-            \MsiClient\Client::POST_REQUEST, $this->getUrl() . "/$userId/permission/sync");
+            FuckAtAll::POST_REQUEST, $this->getUrl() . "/$userId/permission/sync");
     }
 
     public function getByToken($userId, $token)
     {
-        return $this->perform([], \MsiClient\Client::GET_REQUEST, $this->getUrl() . "/$userId/token/$token");
+        return $this->perform([], FuckAtAll::GET_REQUEST, $this->getUrl() . "/$userId/token/$token");
     }
 
     public function setRememberToken($identifier, $token)
     {
-        return $this->perform(['token' => $token], \MsiClient\Client::PUT_REQUEST, $this->getUrl() . "/$identifier/remember-token");
+        return $this->perform(['token' => $token], FuckAtAll::PUT_REQUEST, $this->getUrl() . "/$identifier/remember-token");
     }
 
     public function getByCredentials($email, $password)
@@ -112,6 +112,6 @@ class UserCommand extends Command
             [
                 'email' => $email,
                 'password' => $password,
-            ], \MsiClient\Client::POST_REQUEST, $this->getUrl() . "/by-credentials");
+            ], FuckAtAll::POST_REQUEST, $this->getUrl() . "/by-credentials");
     }
 }
